115ef9ac2f025e3afebec9e4a4c93f61b38eac9d
James Martin <jamesmartin442@gmail.com>
2019-01-08 18:18:14 +0000

    Starting auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect } from 'react';
+import { connect } from 'react-redux';
+import 'firebase/init';
+import firebase from 'firebase/app';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import Header from 'components/layout/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const uiConfig = {
+	signInFlow: 'popup',
+	signInOptions: [
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID
+	],
+	callbacks: {
+		signInSuccessWithAuthResult: (data) => {
+			//
+		}
+	}
+};
+
+function App({ user }) {
+	return (
+		<>
+			{!user && (
+				<StyledFirebaseAuth
+					uiConfig={uiConfig}
+					firebaseAuth={firebase.auth()}
+				/>
+			)}
+
+			{user && (
+				<div className="App">
+					<Header />
+				</div>
+			)}
+		</>
+	);
 }
 
-export default App;
+function mapStateToProps(state) {
+	return {
+		user: state.auth.user
+	};
+}
+
+export default connect(mapStateToProps)(App);
