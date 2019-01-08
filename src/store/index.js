a8c5ea5e6a84a2b25699786e80e4dfa313f1fda9
James Martin <jamesmartin442@gmail.com>
2019-01-08 19:51:40 +0000

    Addewd devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+// import { configureStore } from 'redux-starter-kit';
 import ReduxThunk from 'redux-thunk';
 import auth from 'store/auth';
 
 const rootReducer = combineReducers({ auth });
 
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const store = createStore(
+	rootReducer,
+	compose(
+		applyMiddleware(ReduxThunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
 
-store.subscribe(() => console.log(store.getState()));
+// const store = configureStore({ reducer: rootReducer });
 
 export default store;
