13c11d9e208dbe70d3020f2b45b659b5fc92ba2f
James Martin <jamesmartin442@gmail.com>
2019-01-08 19:51:34 +0000

    Exporting action types now from the store

--- src/store/auth.js
+++ src/store/auth.js
@@ -3,17 +3,28 @@ import produce from 'immer';
 const currentUser = JSON.parse(localStorage.getItem('user'));
 
 const defaultState = {
-	user: currentUser || null
+	user: currentUser || null,
+	loading: true
+};
+
+export const actions = {
+	LOADING: 'LOADING',
+	LOGIN: 'LOGIN',
+	LOGOUT: 'LOGOUT'
 };
 
 export default function auth(state = defaultState, action) {
 	return produce(state, (draft) => {
+		console.log(action);
 		switch (action.type) {
-			case 'LOGIN':
+			case actions.LOGIN:
 				draft.user = action.payload;
 				break;
-			case 'LOGOUT':
+			case actions.LOGOUT:
 				draft.user = null;
+				break;
+			case actions.LOADING:
+				draft.loading = action.payload || false;
 				break;
 			default:
 				break;
