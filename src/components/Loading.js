377b45abc98c8c53cb2df6475f1d32187e51d699
jamesmartin <jamesmartin442@gmail.com>
2019-01-13 17:38:33 +0000

    Fixed memory leak

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 export default function Loading() {
 	const [show, setShow] = useState(false);
+	const timeout = useRef(null);
+	
 	useEffect(() => {
-		setTimeout(() => setShow(true), 1000);
+		timeout.current = setTimeout(() => setShow(true), 1000);
+
+		return () => clearTimeout(timeout.current);
 	}, []);
 	return show && <div>Loading...</div>;
 }
