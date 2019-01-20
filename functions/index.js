c29e3e9cc6c0a1b9b4f3f26027c0c5dd58e5f92d
jamesmartin <jamesmartin442@gmail.com>
2019-01-20 10:43:37 +0000

    Update leagues when a dog is added/removed

--- functions/index.js
+++ functions/index.js
@@ -17,10 +17,41 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 // 		.set({});
 // });
 
-function diffArray(a, b) {
-	return a.filter((n) => b.includes(n));
-}
+exports.updateDogLeagues = functions.firestore
+	.document('leagues/{leagueId}')
+	.onUpdate((change, context) => {
+		const originalData = change.before.data();
+		const newData = change.after.data();
 
-// exports.updateDogLeagues = functions.firestore
-// 	.document('leagues/{leagueId}')
-// 	.onUpdate((change, context) => {});
+		//Something probably went wrong...
+		if (!originalData || !newData) return;
+
+		const leagueId = context.params.leagueId;
+		const originalDogs = originalData.dogs || {};
+		const newDogs = newData.dogs || {};
+
+		const originalDogIds = new Set(Object.keys(originalDogs));
+		const newDogIds = new Set(Object.keys(newDogs));
+
+		//Added dogs are dogs which ARE in new dogs, but are not in original dogs
+		const addedDogs = [...newDogIds].filter(d => !originalDogIds.has(d));
+
+		//Removed dogs are dogs which ARE in original dogs, but are not in new dogs.
+		const removedDogs = [...originalDogIds].filter(d => !newDogIds.has(d));
+
+		const db = admin.firestore();
+
+		addedDogs.forEach(id => {
+			const doc = db.collection('dogs').doc(id);
+			doc.update({
+				leagues: admin.firestore.FieldValue.arrayUnion(leagueId)
+			});
+		});
+
+		removedDogs.forEach(id => {
+			const doc = db.collection('dogs').doc(id);
+			doc.update({
+				leagues: admin.firestore.FieldValue.arrayRemove(leagueId)
+			});
+		});
+	});
