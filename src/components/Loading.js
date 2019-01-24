26478dae2b35e1e036157288dca8079d3634d8b6
James Martin <jamesmartin442@gmail.com>
2019-01-24 18:13:47 +0000

    Show the spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { Spin } from 'antd';
 
 export default function Loading() {
 	const [show, setShow] = useState(false);
 	const timeout = useRef(null);
-	
+
 	useEffect(() => {
 		timeout.current = setTimeout(() => setShow(true), 1000);
 
 		return () => clearTimeout(timeout.current);
 	}, []);
-	return show && <div>Loading...</div>;
+	return show && <Spin />;
 }
