709d2fe9faaaf664a79a0e49db62f737ba9bdb23
James Martin <jamesmartin442@gmail.com>
2019-02-07 19:59:01 +0000

    new functions for various things

--- functions/index.js
+++ functions/index.js
@@ -62,6 +62,8 @@ exports.updateDogLeagues = functions.firestore
 
 		addedDogs.forEach(([id, data]) => {
 			const doc = db.collection('dogs').doc(id);
+			//Set their initial points to 0 so they appear in the league right away
+			data.points = 0;
 			doc.update({
 				leagues: {
 					[leagueId]: data
@@ -81,4 +83,114 @@ exports.updateDogLeagues = functions.firestore
 			added: addedDogs.length,
 			removed: removedDogs.length
 		};
+	});
+
+function convertPlaceToPoints(place) {
+	const pointsMap = {
+		1: 20,
+		2: 19,
+		3: 18,
+		4: 17,
+		5: 16,
+		6: 15,
+		7: 14,
+		8: 13,
+		9: 12,
+		10: 11
+	};
+
+	// any valid place gets 10 points minimum
+	return pointsMap[place] || 10;
+}
+
+exports.dogScoreAdded = functions.firestore
+	.document('dogs/{dogId}/shows/{showId}/runs/{runId}')
+	.onCreate(async (snap, context) => {
+		//Get dog doc
+		const { dogId, runId } = context.params;
+		const dog = admin
+			.firestore()
+			.collection('dogs')
+			.doc(dogId);
+
+		//Add 'runs' object to the specific league entry in users profile, e.g. leagues.2019.runs
+		const runData = snap.data();
+
+		const { league } = runData;
+		const newPoints = convertPlaceToPoints(runData.place);
+
+		//Add a prop in the runs object of [run.id]: points
+		await dog.update({
+			[`leagues.${league}.runs.${runId}`]: newPoints
+		});
+
+		const dogSnap = await dog.get();
+		// const dogData = dogSnap.data();
+		const currentPoints = dogSnap.get(`leagues.${league}.points`) || 0;
+
+		//Add the points score for this run to their total
+		const totalPoints = currentPoints + newPoints;
+		return dog.update({
+			[`leagues.${league}.points`]: totalPoints
+		});
+	});
+
+exports.dogScoreUpdated = functions.firestore
+	.document('dogs/{dogId}/shows/{showId}/runs/{runId}')
+	.onUpdate(async (change, context) => {
+		//Get dog doc
+		const { dogId, runId } = context.params;
+		const dog = admin
+			.firestore()
+			.collection('dogs')
+			.doc(dogId);
+
+		const runData = change.after.data();
+		const { league } = runData;
+
+		//Update the points for this run in the dogs league point score
+		const newPoints = convertPlaceToPoints(runData.place);
+
+		await dog.update({
+			[`leagues.${league}.runs.${runId}`]: newPoints
+		});
+
+		const dogSnap = await dog.get();
+
+		//run through all of the logged points scores and generate a new score for the user
+		const runs = dogSnap.get(`leagues.${league}.runs`);
+		const newTotal = Object.values(runs).reduce((prev, points) => {
+			return (prev += points);
+		}, 0);
+
+		return dog.update({
+			[`leagues.${league}.points`]: newTotal
+		});
+	});
+
+exports.dogScoreDeleted = functions.firestore
+	.document('dogs/{dogId}/shows/{showId}/runs/{runId}')
+	.onDelete(async (snap, context) => {
+		//Get dog doc
+		const { dogId, runId } = context.params;
+		const dog = admin
+			.firestore()
+			.collection('dogs')
+			.doc(dogId);
+
+		//Remove the run from the runs object
+		const runData = snap.data();
+
+		const { league } = runData;
+		const dogSnap = await dog.get();
+		const points = dogSnap.get(`leagues.${league}.runs.${runId}`);
+		const currentPoints = dogSnap.get(`leagues.${league}.points`);
+
+		//Add a prop in the runs object of [run.id]: points
+		//remove the points for this run from the users total
+		return dog.update({
+			[`leagues.${league}.runs.${runId}`]: admin.firestore.FieldValue.delete(),
+			[`leagues.${league}.points`]:
+				parseInt(currentPoints) - parseInt(points)
+		});
 	});
