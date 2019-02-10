af1341ed12add4c1c39ed65e1cbd3f59be76fbc7
James Martin <jamesmartin442@gmail.com>
2019-02-10 17:34:24 +0000

    Firebase functions now recalculate the score everytime, to work around a bug where they would sometimes run twice

--- functions/index.js
+++ functions/index.js
@@ -103,6 +103,14 @@ function convertPlaceToPoints(place) {
 	return pointsMap[place] || 10;
 }
 
+function recalculateScore(league, dogSnap) {
+	//run through all of the logged points scores and generate a new score for the user
+	const runs = dogSnap.get(`leagues.${league}.runs`);
+	return Object.values(runs).reduce((prev, { points }) => {
+		return (prev += points);
+	}, 0);
+}
+
 exports.dogScoreAdded = functions.firestore
 	.document('dogs/{dogId}/shows/{showId}/runs/{runId}')
 	.onCreate(async (snap, context) => {
@@ -130,19 +138,17 @@ exports.dogScoreAdded = functions.firestore
 
 		const dogSnap = await dog.get();
 		// const dogData = dogSnap.data();
-		const currentPoints = dogSnap.get(`leagues.${league}.points`) || 0;
 
 		//Add the points score for this run to their total
-		const totalPoints = currentPoints + newPoints;
+		const recalculatedScore = recalculateScore(league, dogSnap);
 		return dog.update({
-			[`leagues.${league}.points`]: totalPoints
+			[`leagues.${league}.points`]: recalculatedScore
 		});
 	});
 
 exports.dogScoreUpdated = functions.firestore
 	.document('dogs/{dogId}/shows/{showId}/runs/{runId}')
 	.onUpdate(async (change, context) => {
-		//Get dog doc
 		const { dogId, runId, showId } = context.params;
 		const dog = admin
 			.firestore()
@@ -165,40 +171,35 @@ exports.dogScoreUpdated = functions.firestore
 
 		const dogSnap = await dog.get();
 
-		//run through all of the logged points scores and generate a new score for the user
-		const runs = dogSnap.get(`leagues.${league}.runs`);
-		const newTotal = Object.values(runs).reduce((prev, { points }) => {
-			return (prev += points);
-		}, 0);
+		const recalculatedScore = recalculateScore(league, dogSnap);
 
 		return dog.update({
-			[`leagues.${league}.points`]: newTotal
+			[`leagues.${league}.points`]: recalculatedScore
 		});
 	});
 
 exports.dogScoreDeleted = functions.firestore
 	.document('dogs/{dogId}/shows/{showId}/runs/{runId}')
 	.onDelete(async (snap, context) => {
-		//Get dog doc
 		const { dogId, runId } = context.params;
 		const dog = admin
 			.firestore()
 			.collection('dogs')
 			.doc(dogId);
 
-		//Remove the run from the runs object
 		const runData = snap.data();
 
 		const { league } = runData;
-		const dogSnap = await dog.get();
-		const points = dogSnap.get(`leagues.${league}.runs.${runId}.points`);
-		const currentPoints = dogSnap.get(`leagues.${league}.points`);
 
-		//Add a prop in the runs object of [run.id]: points
-		//remove the points for this run from the users total
+		//Remove the run from the runs object
+		await dog.update({
+			[`leagues.${league}.runs.${runId}`]: admin.firestore.FieldValue.delete()
+		});
+
+		const dogSnap = await dog.get();
+		const recalculatedScore = recalculateScore(league, dogSnap);
+
 		return dog.update({
-			[`leagues.${league}.runs.${runId}`]: admin.firestore.FieldValue.delete(),
-			[`leagues.${league}.points`]:
-				parseInt(currentPoints) - parseInt(points)
+			[`leagues.${league}.points`]: recalculatedScore
 		});
 	});
