4d144f33e90d05a900449df61cbbf1d1be6e9a11
James Martin <jamesmartin442@gmail.com>
2019-03-19 20:21:15 +0000

    Updated packages, the league table should order properly now

--- src/components/tables/LeagueTable.js
+++ src/components/tables/LeagueTable.js
@@ -11,8 +11,8 @@ export default function LeagueTable({ league, minGrade, maxGrade }) {
 		.collection('dogs')
 		.where(`leagues.${league}.grade`, '>=', minGrade)
 		.where(`leagues.${league}.grade`, '<=', maxGrade)
-		.orderBy(`leagues.${league}.grade`, 'desc')
-		.orderBy(`leagues.${league}.points`, 'desc');
+		.orderBy(`leagues.${league}.grade`, 'desc');
+	// .orderBy(`leagues.${league}.points`, 'desc');
 
 	const competitors = useCollection(query);
 
@@ -24,6 +24,15 @@ export default function LeagueTable({ league, minGrade, maxGrade }) {
 		key: doc.id,
 		index: index + 1
 	}));
+
+	/*
+		There is a significant limitation to firestore, where if you query with a `where`, the first orderBy MUST be the same column as that where.
+		This means that, in this case, dogs in a higher grade will _always_ appear above dogs with a lower grade, _regardless of whether or not they have more points_.
+		This is a ridiculous requirement, and it means that I must do the sorting here, rather than in the query.
+	*/
+	data.sort((a, b) => {
+		return a.leagues[league].points > b.leagues[league].points ? -1 : 1;
+	});
 
 	const columns = [
 		{
